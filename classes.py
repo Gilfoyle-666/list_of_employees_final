b8bbc591442a3b5135d994f943710bd56e426521
Gilfoyle-666 <bertramgilfoyle@tutanota.com>
2024-04-19 20:04:33 +0300

    added connection in start app

--- classes.py
+++ classes.py
@@ -5,3 +5,19 @@ import tkinter as tk
 from tkinter import ttk
 #import tkinter.ttk as ttk
 import re
+
+class main:
+	def __init__(self,title="Учет сотрудников"):
+		self.root = Tk()
+		self.root.title(title)
+		self.root.geometry("300x550")
+		# создание соединения с базой данных и извлечение данных при открытии приложения
+		self.connection = sqlite3.connect('database.db')
+		self.cur = self.connection.cursor()
+		self.query = """ CREATE TABLE IF NOT EXISTS employees(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL, date_of_exam) """
+		self.cur.execute(self.query)
+		self.cur.execute("SELECT * FROM employees")
+		self.results =self.cur.fetchall()
+		self.connection.commit()
+		self.cur.close()
+		self.connection.close()
